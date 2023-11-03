e2438e93e7f326824426d96214c3a3c92d2fb3ed
AzureAD\VinceErkadoo <vince@zapproach.com>
2023-11-03 13:18:50 +0400

    Initial commit

---
+++ src/config/roles.js
@@ -0,0 +1,13 @@
+const allRoles = {
+  user: [],
+  vendor: ['getUsers', 'manageUsers'],
+  admin: ['getUsers', 'manageUsers'],
+};
+
+const roles = Object.keys(allRoles);
+const roleRights = new Map(Object.entries(allRoles));
+
+module.exports = {
+  roles,
+  roleRights,
+};
